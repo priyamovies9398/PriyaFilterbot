206632631a011c3282c64f827fd048fda6e9f8f2
SarukuBots <158731094+priyamovies9398@users.noreply.github.com>
2024-02-03 22:46:16 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 25209455
+API_HASH = 'a98a05c04b469f8ed80e1e543c84e515'
+BOT_TOKEN = '5694117908:AAEK4OGIc2-TVx2zKU26W-CF1Jy0vUI_TfU'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -12,13 +12,13 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+CHANNELS = [-1001979592528, -1001819052226, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'GreyMatter'
+DATABASE_URI = "mongodb+srv://saruku123:saruku123@cluster0.gdljzd8.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'saruku123'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 

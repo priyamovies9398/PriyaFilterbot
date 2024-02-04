75fcdc084648a4c552f2d04d153f6f6fbf8bd1a9
SarukuBots <158731094+priyamovies9398@users.noreply.github.com>
2024-02-04 19:05:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'http://FlashshortX.in/api'
+    url = f'http://earnpro.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }

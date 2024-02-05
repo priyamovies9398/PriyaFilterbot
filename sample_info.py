ea369392a291c518e62d4005b61b735333fbc104
1206moviesclub <153907712+1206moviesclub@users.noreply.github.com>
2024-02-06 01:56:01 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 25209455
-API_HASH = 'a98a05c04b469f8ed80e1e543c84e515'
-BOT_TOKEN = '5694117908:AAEK4OGIc2-TVx2zKU26W-CF1Jy0vUI_TfU'
+API_ID = 3754240
+API_HASH = 'd42230fdbe182a73dfe9f5f57588e833'
+BOT_TOKEN = '6314046384:AAEya-6saIQGTPcL0ABH28va7T6mfoxm3xw'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -12,13 +12,13 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-1001979592528, -1001819052226, 'channelusername']
+CHANNELS = [-1001528296283, -1002053184016, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://saruku123:saruku123@cluster0.gdljzd8.mongodb.net/?retryWrites=true&w=majority"
-DATABASE_NAME = 'saruku123'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://1206moviesclub:<1206moviesclub>@1206moviesclub.a23fgtk.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = '1206moviesclub'
+COLLECTION_NAME = 'MC_files'  # If you are using the same database, then use different collection name for each bot
 
 

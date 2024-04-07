27b6755d97fd8efc3a8e46cb3065f7bb9155f9c1
SarukuBots <158731094+priyamovies9398@users.noreply.github.com>
2024-04-07 23:46:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'http://earnpro.in/api'
+    url = f'http://pdiskshortener.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
